ec9db42c4006e54417d35e1593ef06590a7dc947
Ibnu Gayuh Fadilah <ibnugayuhfadilah@gmail.com>
2026-02-22 11:33:51 +0700

    Siap untuk portofolio online

---
+++ mysite/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views
+from blog import views as blog_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('', include('blog.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
